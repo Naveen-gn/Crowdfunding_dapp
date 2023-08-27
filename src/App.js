424f6239be80a850757ba32f869bfe4e6cd532e0
Naveen <naveen2003gn@gmail.com>
2023-08-27 22:09:37 +0530

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>C.kuzhanthai velu </h1>
+    <h1>crowdfunding app</h1>
+        <h1>web 3 </h1>
+    </div>
+  );
+}
+
+export default App;
