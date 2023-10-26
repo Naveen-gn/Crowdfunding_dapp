d5c70c3b8795d84e5f6ef4a5882b768f77bffe99
Naveen <116023649+Naveen-gn@users.noreply.github.com>
2023-10-26 11:43:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>C.kuzhanthai velu </h1>
+    <h1>G.Naveen </h1>
     <h1>crowdfunding app</h1>
         <h1>web 3 </h1>
     </div>
